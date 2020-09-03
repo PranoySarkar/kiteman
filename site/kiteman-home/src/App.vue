26eb395133afa3b6715e51324ebfc8825e73bad4
prnysarker <pranoy.sarkar@softwareag.com>
2020-09-03 10:15:43 +0530

    home page update

--- site/kiteman-home/src/App.vue
+++ site/kiteman-home/src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
-   
-   <router-view></router-view>
+    <header>
+      <h1>KiteMan</h1>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: "App",
-  components: {
-
-  },
-    mounted: function () {
-      document.title="KiteMan"
+  components: {},
+  mounted: function () {
+    document.title = "KiteMan";
   },
 };
 </script>
 
 <style>
-html{
+html {
   font-size: 62.5%;
 }
 * {
@@ -35,5 +35,18 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+header {
+    /* background: linear-gradient(83deg, #393737, #837777); */
+    /* border-bottom: 0px solid #ff9800; */
+    border-right: 10px solid #8191a4c7;
+    box-shadow: 3px 3px 6px 3px #6c6e7a38;
+    overflow: hidden;
+    padding: 0 0 0 10px;
+    color: #f6542b;
+    width: 43vw;
+    border-bottom-right-radius: 80px;
+    min-width: 200px;
 }
 </style>
--- site/kiteman-home/src/components/HomeComponent.vue
+++ site/kiteman-home/src/components/HomeComponent.vue
@@ -1,18 +1,90 @@
 <template>
-    <div></div>    
+  <div id="homeContainer">
+    <div id="hero">
+      <div class="imageContainer">
+        <img src="../assets/hero.png" alt />
+      </div>
+      <div class="lecture">
+        <div>
+          <a
+            class="callToActionLink"
+            target="_blank"
+            href="https://chrome.google.com/u/1/webstore/devconsole/b7258b33-d49c-432a-8930-e8c23fca9bde?hl=en"
+          >Install Now</a>
+        </div>
+      </div>
+    </div>
+    <div class="eachFeature"></div>
+  </div>
 </template>
 
 
 <script >
-
-export default{
-    name:'Home',
-}
+export default {
+  name: "Home",
+};
 </script>
 
 
 <style  scoped>
-    div{
-        color :green;
-    }
+#hero {
+  max-height: 80vh;
+  overflow: hidden;
+
+  max-width: 80vw;
+  margin: auto;
+  padding: 10px;
+  margin-top: 20px;
+  display: flex;
+}
+
+img {
+  height: 100%;
+  width: auto;
+}
+
+.imageContainer {
+  height: 75vh;
+  border: 1px solid #b9b8b8;
+  border-radius: 2px;
+  display: inline-block;
+  padding: 10px;
+  box-shadow: 0 0 7px 3px #c9c9c966;
+}
+
+.lecture {
+  /* background: aliceblue; */
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 20px;
+}
+
+.callToActionLink {
+  background: white;
+  padding: 16px 80px;
+  /* min-width: 127px; */
+  border: 1px solid #2c3e5066;
+  border-radius: 2px;
+  text-decoration: none;
+  color: inherit;
+  font-size: 3rem;
+  box-shadow: -1px -2px 20px #bbadad4f;
+  background: linear-gradient(45deg, #ffffff, #eceff3);
+  transition: all 0.05s;
+  /* color: #FFF; */
+}
+
+.callToActionLink:hover {
+  box-shadow: -1px -2px 10px #bbadad4f;
+
+  /* color: #FFF; */
+}
+
+.callToActionLink:active {
+  box-shadow: unset;
+
+  /* color: #FFF; */
+}
 </style>
